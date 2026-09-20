83283afe4a5ce2beb654b7502d6429af69a9e8ad
zhenzhen030413-arch <zhenzhen030413@gmail.com>
2026-09-20 15:50:15 +0800

    Multi-Step Agent

---
+++ week03/day03/tool_schemas.py
@@ -0,0 +1,87 @@
+# 创建工具列表。
+# 后面可以不断往这个列表里面增加新的 Tool。
+TOOLS = [
+
+    # 第一个工具。
+    {
+        # 告诉模型，这是一个函数类型的工具。
+        "type": "function",
+
+        # function 中描述具体函数。
+        "function": {
+
+            # Python 函数名称。
+            "name": "get_ticket",
+
+            # 告诉 LLM 这个函数是干什么的。
+            "description": "根据工单编号查询工单的问题、优先级和处理状态",
+
+            # 定义调用函数时需要哪些参数。
+            "parameters": {
+
+                # 参数整体是一个 JSON Object。
+                "type": "object",
+
+                # properties 用于定义每一个参数。
+                "properties": {
+
+                    # ticket_id 就是我们的函数参数。
+                    "ticket_id": {
+
+                        # ticket_id 的数据类型是字符串。
+                        "type": "string",
+
+                        # 参数解释同样会提供给 LLM。
+                        "description": "需要查询的工单编号，例如 T1001"
+                    }
+                },
+
+                # required 表示哪些参数必须存在。
+                "required": ["ticket_id"]
+            }
+        }
+    },
+    # 第二个工具。
+    {#根据用户的关键词搜索相关工单,当用户不知道具体的工单编号时，可以使用这个工具来搜索相关工单。
+        "type": "function",
+        "function":{
+            "name": "search_ticket",
+            "description": " 根据关键词搜索，只返回工单编号和问题描述，不包含状态、优先级。"
+            "1. keyword 参数只能提取【核心名词】，禁止附带“相关、有关、对应的”这类修饰词"
+"2. 例如用户说“找登录相关工单”，keyword只能输出：登录"
+"3. 只保留业务实体词，不要多余修饰词,",
+            "parameters": {
+                "type": "object",
+                "propertys": {
+                    "keyword":{
+                        "type": "string",
+                        "description":"需要查询的关键词，例如 用户无法登录系统" ,
+                    }
+                },
+                "required": ["keyword"],
+            }
+            
+        }
+
+    },
+    {
+        "type": "function",
+
+        "function": {
+            "name": "get_user_ticket",
+            "description": "根据用户名查询该用户的所有工单",
+            "parameters": {
+                "type": "object",
+
+                "properties": {
+                    "user_name": {
+                        "type": "string",
+                        "description": "需要查询的用户名，例如 张三"
+                    }
+                },
+
+                "required": ["user_name"]
+            }
+        }
+    }
+]
